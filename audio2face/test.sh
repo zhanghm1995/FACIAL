9d175ff7840e7ab7dfc0c7fa3f6eacb0aadaec12
zhanghm1995 <zhanghm_1995@qq.com>
2022-01-05 15:06:40 +0800

    update audio2fac

---
+++ audio2face/test.sh
@@ -0,0 +1,7 @@
+set -x
+## Example
+# python test.py --audiopath ../examples/audio_preprocessed/test1.pkl \
+#                --checkpath ./checkpoint/train1/Gen-90.mdl
+
+python test.py --audiopath ../examples/audio_preprocessed/gangqiang-3_30fps.pkl \
+               --checkpath ./checkpoint/gangqiang/Gen-105.mdl
